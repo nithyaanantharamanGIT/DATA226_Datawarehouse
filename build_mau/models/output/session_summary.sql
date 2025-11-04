2013ed3992c859ad54386418e0b37b8d805cde1d
Nithya Anantharaman <niths1994@gmail.com>
2025-11-03 22:53:10 -0800

    Update session_summary.sql

--- build_mau/models/output/session_summary.sql
+++ build_mau/models/output/session_summary.sql
@@ -1,7 +1,14 @@
+WITH user_session_data AS (
+    SELECT * FROM {{ ref("user_session_channel") }}
+),
+session_timestamp_data AS (
+    SELECT * FROM {{ ref("session_timestamp") }}
+)
 SELECT 
-    u.userId, 
-    u.sessionId, 
-    u.channel, 
-    st.ts
-FROM {{ ref("user_session_channel") }} u
-JOIN {{ ref("session_timestamp") }} st ON u.sessionId = st.sessionId
+    user_session_data.userId, 
+    user_session_data.sessionId, 
+    user_session_data.channel, 
+    session_timestamp_data.ts
+FROM user_session_data
+JOIN session_timestamp_data ON user_session_data.sessionId = session_timestamp_data.sessionId
+EOF
